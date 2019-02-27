45e5e4817f0453b48cc7a1888182d34083ee9288
Ramon Geronimo <rgero215@gmail.com>
2019-02-27 01:27:51 -0800

    modified app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import quotes
 from flask import (Flask, render_template, redirect, 
                     url_for, make_response, flash, request)
 from histogram_dictionary import *
-import markov_chain
 
 app = Flask(__name__)
 
@@ -12,15 +11,17 @@ app.secret_key = 'ldfasndohsdnafasngoherqowr'
 
 @app.route('/')
 def home():
-    init = quotes.initialize()
-    menu = quotes.menu_loop()
     sentence1 = []
     sentence2 = []
     likes = 0
-    for i in range(5, 20):
-        sentence1.append(sample(histogram('Frankenstein.txt')))
-        sentence2.append(weighted_random(histogram('Frankenstein.txt')))
-    return render_template("index.html", sentence1 = ' '.join(sentence1), sentence2 = ' '.join(sentence2), likes=str(likes), saves=get_saved_data())
+    sentence1.append(markov_chain(histogram('Frankenstein.txt')))
+    sentence2.append(markov_chain(histogram('Frankenstein.txt')))
+    quotes.Quote.create(
+            quote_one = ' '.join(sentence1),
+            quote_two = ' '.join(sentence2),
+            likes = likes,
+        )
+    return render_template("index.html", sentence1 = ' '.join(sentence1), sentence2 = ' '.join(sentence2), likes=str(likes))
 
 @app.route('/index')  
 def index():
@@ -30,27 +31,12 @@ def index():
     sentence2 = []
     likes = 0
     add_quote = quotes.add_quote()
-    # for i in range(5, 20):
-    #     sentence1.append(sample(histogram('Frankenstein.txt')))
-    #     sentence2.append(weighted_random(histogram('Frankenstein.txt')))
+
     sentence1.append(markov_chain(histogram('Frankenstein.txt')))
     sentence2.append(markov_chain(histogram('Frankenstein.txt')))
     return render_template("index.html", sentence1 = ' '.join(sentence1), sentence2 = ' '.join(sentence2), likes=str(likes), saves=get_saved_data())
 
-
-def get_saved_data():
-    try:
-        data = json.loads(request.cookies.get('quotes'))
-    except TypeError:
-        data = {}
-    return data
-
-@app.route('/save', methods=['POST'])
-def save():
-    response = make_response(redirect(url_for('index')))
-    data = get_saved_data()
-    data.update(dict(request.form.items()))
-    response.set_cookie('quotes', json.dumps(data))
-    return response
-
-app.run(debug=True)
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
