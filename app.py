342243dfaa4a7984f667e420f57819d53e446c25
Ramon Geronimo <rgero215@gmail.com>
2019-02-27 01:12:29 -0800

    add markov_chain to heroku app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import quotes
 from flask import (Flask, render_template, redirect, 
                     url_for, make_response, flash, request)
 from histogram_dictionary import *
+import markov_chain
 
 app = Flask(__name__)
 
@@ -29,9 +30,11 @@ def index():
     sentence2 = []
     likes = 0
     add_quote = quotes.add_quote()
-    for i in range(5, 20):
-        sentence1.append(sample(histogram('Frankenstein.txt')))
-        sentence2.append(weighted_random(histogram('Frankenstein.txt')))
+    # for i in range(5, 20):
+    #     sentence1.append(sample(histogram('Frankenstein.txt')))
+    #     sentence2.append(weighted_random(histogram('Frankenstein.txt')))
+    sentence1.append(markov_chain(histogram('Frankenstein.txt')))
+    sentence2.append(markov_chain(histogram('Frankenstein.txt')))
     return render_template("index.html", sentence1 = ' '.join(sentence1), sentence2 = ' '.join(sentence2), likes=str(likes), saves=get_saved_data())
 
 
